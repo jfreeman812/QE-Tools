827445ffccab99ea4d0e3c10aed95d95e05dcfd1
Lewis Franklin <lewis.franklin@rackspace.com>
2017-05-31 18:15:47 -0500

    QERBA-812 (FF): Install Correct Behave
    In the absence of proper clone-from-git support in PBR, install behave
    as a requirement. A hack, but at least it works.

--- env-setup.sh
+++ env-setup.sh
@@ -6,7 +6,8 @@ if [ -z "${VIRTUAL_ENV+x}" ] ; then
 fi
 echo Setting up "$VIRTUAL_ENV"
 echo
-pip install -U flake8 "pbr==1.10.0"
+pip install -U flake8
+pip install -U -r requirements.txt
 for extra in "${@:1}"; do
     pip install -e .[$extra]
 done
