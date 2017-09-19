22f3c7d1f14ab7ae68abc8f6d02cd13b96187016
Shawn Dutton <shawn.dutton@rackspace.com>
2017-09-19 09:30:01 -0500

    QERBA-1463: run_quarantine_reportssupport one repo

--- run_quarantine_reports.sh
+++ run_quarantine_reports.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Generate quarantined reports for every repo given as an argument.
+# Generate quarantined reports for the given repo name, and interface type.  If desired provide
+# optional product directory specifier with -p <product_dir_name>
 
 search_hidden=""
 if [ "$1" = "--search_hidden" ]; then
@@ -8,24 +9,24 @@ if [ "$1" = "--search_hidden" ]; then
     shift
 fi
 
-# Create a temporary directory for the repos we want to run the reports on to be cloned into.
+repo_name="$1"
+shift
+
+# Create a temporary directory for the repo we want to run the reports on to be cloned into.
 temp_dir=$(mktemp -d)
 
 working_dir=$(pwd)
 cd "$temp_dir"
 
-# Clone the repos into a temp directory.
-for repo_name in "$@"
-do
-    git clone "git@github.rackspace.com:"/"$repo_name".git --depth 1
-done
+# Clone the repo into a temp directory.
+git clone "git@github.rackspace.com:"/"$repo_name".git --depth 1
 
 cd "$working_dir"
 
-# Process a report for every repo cloned
+# Process the reports for the repo.
 for repo_dir in "$temp_dir"/*
 do
-    ./run_quarantine_reports_on_repo.py "$repo_dir"$search_hidden
+    ./run_quarantine_reports_on_repo.py "$repo_dir" "$@""$search_hidden"
 done
 
 # Cleanup the temp directory
