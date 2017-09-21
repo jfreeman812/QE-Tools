9461c792b1fa39a83d275784953e83116883d31b
Shawn Dutton <shawn.dutton@rackspace.com>
2017-09-21 16:27:52 -0500

    QERBA-1469: Bash cleanup

--- run_quarantine_reports.sh
+++ run_quarantine_reports.sh
@@ -3,15 +3,6 @@
 # Generate quarantined reports for the given repo name, and interface type.  If desired provide
 # optional product directory specifier with -p <product_dir_name>
 
-search_hidden=""
-if [ "$1" = "--search_hidden" ]; then
-    search_hidden=" --search_hidden"
-    shift
-fi
-
-repo_name="$1"
-shift
-
 # Create a temporary directory for the repo we want to run the reports on to be cloned into.
 temp_dir=$(mktemp -d)
 
@@ -19,14 +10,17 @@ working_dir=$(pwd)
 cd "$temp_dir"
 
 # Clone the repo into a temp directory.
-git clone "git@github.rackspace.com:"/"$repo_name".git --depth 1
+git clone "git@github.rackspace.com:"/"$1".git --depth 1
+
+# Remove the 1st argument for the repo name, as it is no longer needed.
+shift
 
 cd "$working_dir"
 
 # Process the reports for the repo.
 for repo_dir in "$temp_dir"/*
 do
-    ./run_quarantine_reports_on_repo.py "$repo_dir" "$@""$search_hidden"
+    ./run_quarantine_reports_on_repo.py "$repo_dir" "$@"
 done
 
 # Cleanup the temp directory
