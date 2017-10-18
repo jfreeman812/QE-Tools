e9ce93986394b12782c4d047e279117c0b146cd5
Lewis Franklin <lewis.franklin@rackspace.com>
2017-10-17 22:37:57 -0500

    Enhancement: Install Flake8 Plugins
    Forgot to update env-setup.sh to use the development.txt file

--- env-setup.sh
+++ env-setup.sh
@@ -6,8 +6,8 @@ if [ -z "${VIRTUAL_ENV+x}" ] ; then
 fi
 echo Setting up "$VIRTUAL_ENV"
 echo
-pip install -U flake8
 pip install -U -r requirements.txt
+pip install -U -r development.txt
 for extra in "${@:1}"; do
     pip install -e .[$extra]
 done
