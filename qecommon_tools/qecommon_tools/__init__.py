8e1a87ab48f02c84870299974218948ea192db12
Lewis Franklin <lewis.franklin@rackspace.com>
2018-01-03 12:11:27 -0600

    Enhancement: Enhanced docstrings; cleaner code
    Migrated to Google-style docstrings and cleaned up the code as a result.

--- qecommon_tools/qecommon_tools/__init__.py
+++ qecommon_tools/qecommon_tools/__init__.py
@@ -3,27 +3,36 @@ import os as _os
 import string as _string
 
 
-def _name_from_file(root, category):
-    'Given root and category, return the contents of display_name.txt if it exists'
-    name_path = _os.path.join(root, category, 'display_name.txt')
+def _name_from_file(path):
+    '''Return the contents of a display_name.txt file if located in the path.
+
+    Args:
+        path (str):  Path for searching
+
+    Returns:
+        str: The contents of display_name.txt if found, otherwise an empty string
+    '''
+    name_path = _os.path.join(path, 'display_name.txt')
     if _os.path.exists(name_path):
         with open(name_path, 'r') as name_fo:
             return name_fo.read().rstrip('\r\n')
+    return ''
 
 
-def display_name(root, category, category_name=None):
+def display_name(path, package_name=''):
+    '''Determine the display name for a project given the provided information. If a file named
+    display_name.txt is found in the folder, use it's contents for the display name.  Otherwise,
+    return a title-cased string from either the base directory or package_name (if provided)
+
+    Args:
+        path (str): Path for searching
+        package_name (str): Sphinx-style, dot-delimited package name (optional)
     '''
-    Determine the display name for a category, given a root path and optional category name. If a
-    file named display_name.txt is found in the root folder, use that for the display name.
-    Otherwise, return a title-cased string from either category or category_name (if provided)
-    '''
-    name_from_file = _name_from_file(root, category)
+    name_from_file = _name_from_file(path)
     if name_from_file:
         return name_from_file
-
-    if category_name:
-        category = category_name.split('.')[-1]
-    return _string.capwords(category.replace('_', ' '))
+    raw_name = package_name.split('.')[-1] if package_name else _os.path.basename(path)
+    return _string.capwords(raw_name.replace('_', ' '))
 
 
 def padded_list(iterable, size, padding=None):
