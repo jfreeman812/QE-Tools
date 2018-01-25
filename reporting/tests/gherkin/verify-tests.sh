e82267bae75aa00404710bdbc63642f0159c6009
Lewis Franklin <lewis.franklin@rackspace.com>
2018-01-25 08:33:49 -0600

    Fix: Use subshell syntax
    Use subshell syntax for cleaner code.

--- reporting/tests/gherkin/verify-tests.sh
+++ reporting/tests/gherkin/verify-tests.sh
@@ -1,8 +1,5 @@
 set -e
-cd good
-coverage-gherkin api --dry-run
-cd ../bad
-coverage-gherkin api --dry-run | sed -e 's/^[^:]*://' > ../actual_output.txt
-cd ..
+(cd good; coverage-gherkin api --dry-run)
+(cd bad; coverage-gherkin api --dry-run | sed -e 's/^[^:]*://') > actual_output.txt
 diff expected_output.txt actual_output.txt
 rm actual_output.txt
