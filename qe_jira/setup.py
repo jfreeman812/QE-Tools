a2b68814b5a3f3db6ba57a84ba60644eaa575cf3
Brad Brown <brad.brown@rackspace.com>
2018-02-12 07:45:34 -0600

    QET-23: version bump

--- qe_jira/setup.py
+++ qe_jira/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='qe_jira',
-    version='1.0.3',
+    version='1.0.4',
     description='Simple helper to create a QE Test JIRA from a dev JIRA',
     author='RBA QE',
     author_email='rba-qe@rackspace.com',
