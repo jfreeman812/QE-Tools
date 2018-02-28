d7bdd09870805f32676c05356ff943d65371d825
Brad Brown <brad.brown@rackspace.com>
2018-02-28 16:56:43 -0600

    QET-40: refactor args and date limit handling

--- reporting/qe_coverage/coverage_historical_repo.py
+++ reporting/qe_coverage/coverage_historical_repo.py
@@ -28,13 +28,13 @@ def subdirectory_path(output_path, date):
 
 
 def generate_rev_dates(args):
-    first_date = time_ago(args.start_unit, args.start_delta)
+    first_date = datetime.date(1970, 1, 1)
+    if hasattr(args, 'start_delta'):
+        first_date = time_ago(args.start_unit, args.start_delta)
     cursor_date = datetime.date.today()
-    dates_to_report = []
     while cursor_date > first_date:
-        dates_to_report.append(cursor_date)
+        yield cursor_date
         cursor_date = time_ago(args.by_unit, 1, start=cursor_date)
-    return dates_to_report
 
 
 def _prepare_coverage_args(coverage_args, output_dir, date):
@@ -47,14 +47,33 @@ def _prepare_coverage_args(coverage_args, output_dir, date):
 
 
 def _allowed_units(unit):
+    if not unit:
+        return VALID_UNITS
     return VALID_UNITS[VALID_UNITS.index(unit) + 1:]
+
+
+class StartUnit(argparse.Action):
+    def __call__(self, parser, args, value, option_string=None):
+        values = value.split()
+        if len(values) != 2:
+            message = '`start-delta` should be only 2 items long (delta and unit): {}'
+            raise ValueError(message.format(values))
+        delta, unit = values
+        if not delta.isdigit():
+            raise ValueError('The `start-delta` delta must be an integer: {}'.format(delta))
+        delta = int(delta)
+        if unit not in VALID_UNITS:
+            message = 'The `start-delta` unit {} was not one of: {}.'
+            raise ValueError(message.format(unit, VALID_UNITS))
+        setattr(args, 'start_delta', delta)
+        setattr(args, 'start_unit', unit)
 
 
 class SubordinateUnit(argparse.Action):
     def __call__(self, parser, args, values, option_string=None):
         start_unit = args.start_unit
-        if values not in _allowed_units(start_unit):
-            message = 'The by unit ({}) must be smaller than the start unit ({})'
+        if start_unit and values not in _allowed_units(start_unit):
+            message = 'The `by-unit` ({}) must be smaller than the start unit ({})'
             raise ValueError(message.format(values, start_unit))
         setattr(args, self.dest, values)
 
@@ -65,15 +84,20 @@ def main():
         description='Build historical coverage reports over a span of time.',
         epilog=epilog
     )
-    parser.add_argument('start_delta', type=int,
-                        help='Integer representing the number of "start_unit"s to reach back to.')
-    parser.add_argument('start_unit', choices=VALID_UNITS,
-                        help='The unit of measure for how far back to start')
+    parser.add_argument('--start-delta', action=StartUnit, dest='start_unit', default=None,
+                        metavar='"<measure> <unit>"',
+                        help='The space-separated (quoted) measure and unit '
+                             'representing how far to look back in the repo history.'
+                             ' The measure must be an integer.'
+                             ' The unit must be one of: {}'
+                             ' E.g. "5 weeks"'.format(VALID_UNITS))
     parser.add_argument('--by-unit', choices=VALID_UNITS, action=SubordinateUnit, default=None,
                         help='The unit of measure for the history increment "slices".'
                              ' Must be a smaller unit that the start unit.'
-                             ' Default value is 1 unit of size smaller than the start unit.')
-    parser.add_argument('--output-dir', default='reports')
+                             ' Default value is 1 unit of size smaller than the start unit '
+                             ' or {} if no start unit provided.'.format(VALID_UNITS[0]))
+    parser.add_argument('--output-dir', default='reports',
+                        help='The relative path from repo root to store the reports.')
     args, coverage_args = parser.parse_known_args()
     depth = subprocess.check_output(['git', 'rev-list', '--count', 'HEAD'],
                                     universal_newlines=True).strip('\n')
@@ -82,7 +106,8 @@ def main():
     output_path = os.path.abspath(args.output_dir)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    for date in generate_rev_dates(args):
+    rev_dates = generate_rev_dates(args)
+    for date in rev_dates:
         rev_command = [
             'git',
             'rev-list',
