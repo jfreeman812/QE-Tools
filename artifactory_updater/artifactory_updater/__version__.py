4eae01a2037af00a827af876fb96520acff9309f
Brad Brown <brad.brown@rackspace.com>
2018-03-15 14:02:10 -0500

    Enhancement: return to standalone script

--- artifactory_updater/artifactory_updater/__version__.py
+++
@@ -1,3 +0,0 @@
-VERSION = (1, 0, 0)
-
-__version__ = '.'.join([str(x) for x in VERSION])
--- artifactory_updater/updater.py
+++ artifactory_updater/updater.py
@@ -2,24 +2,63 @@
 
 import argparse
 from glob import glob
+import json
 import os
+from pkg_resources import parse_version
+from shutil import rmtree
 import subprocess
 import sys
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
 
 
+ARTIFACTORY_DOMAIN = 'artifacts.rackspace.net'
 PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 
-def check_and_update(container_dir, update=True):
-    print('Checking {}...'.format(container_dir))
-    command = [
+def _get_setup_info(container_dir, flag):
+    return subprocess.check_output([
         sys.executable,
         '{}/setup.py'.format(container_dir),
-        'artifactory'
-    ]
-    if not update:
-        command.append('--dry-run')
-    subprocess.call(command, cwd=PARENT_DIR)
+        '--{}'.format(flag)
+    ], universal_newlines=True, cwd=PARENT_DIR).strip('\n')
+
+
+def _latest_artifactory_version(package_name):
+    package_url = 'https://{0}/artifactory/api/storage/{1}-pypi-local/{1}'
+    response = urlopen(package_url.format(ARTIFACTORY_DOMAIN, package_name))
+    package_data = json.loads(response.read())
+    versions = [x['uri'].lstrip('/') for x in package_data['children']]
+    return max(versions, key=parse_version)
+
+
+def _update(container_dir, pkg_name):
+    dist_path = os.path.join(PARENT_DIR, container_dir)
+    print('Removing previous builds...')
+    rmtree(os.path.join(dist_path, 'dist'), ignore_errors=True)
+    rmtree(os.path.join(dist_path, 'build'), ignore_errors=True)
+    print('Building and uploading sdist...')
+    sdist_command = '{} setup.py sdist upload -r {{}}'.format(sys.executable)
+    subprocess.call(sdist_command.format(pkg_name).split(), cwd=dist_path)
+    print('Building and uploading wheel...')
+    bdist_command = '{} setup.py bdist_wheel --universal upload -r {{}}'.format(sys.executable)
+    subprocess.call(bdist_command.format(pkg_name).split(), cwd=dist_path)
+
+
+def check_and_update(container_dir, update=True):
+    pkg_name = _get_setup_info(container_dir, 'name')
+    print('Checking {}...'.format(pkg_name))
+    pkg_version = _get_setup_info(container_dir, 'version')
+    artifactory_version = _latest_artifactory_version(pkg_name)
+    if parse_version(pkg_version) <= parse_version(artifactory_version):
+        print('{} is up to date!'.format(pkg_name))
+        return
+    message = '{} needs to be updated: Current Version: {}; Latest Artifactory Version: {}'
+    print(message.format(pkg_name, pkg_version, artifactory_version))
+    if update:
+        _update(container_dir, pkg_name)
 
 
 def get_packages(dir_list=None):
