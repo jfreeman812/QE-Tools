e3d6b1f08d48bac9b99615c997aeacb4890a9243
Brad Brown <brad.brown@rackspace.com>
2018-03-15 15:36:02 -0500

    Enhancement: restore dummy pkg

---
+++ artifactory_updater/setup.py
@@ -0,0 +1,43 @@
+import os
+import setuptools
+import sys
+
+NAME = 'artifactory_updater'
+DESCRIPTION = 'Helper to check and update a package within Artifactory'
+VERSION = None
+
+CONSOLE_SCRIPTS = [
+]
+
+INSTALL_REQUIRES = [
+]
+
+EXTRAS_REQUIRE = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+setuptools.setup(name=NAME,
+                 version=about['__version__'],
+                 description=DESCRIPTION,
+                 url='https://github.rackspace.com/QualityEngineering/QE-Tools',
+                 author='Rackspace QE',
+                 author_email='qe-tools-contributors@rackspace.com',
+                 license='MIT',
+                 entry_points={
+                     'console_scripts': CONSOLE_SCRIPTS,
+                 },
+                 install_requires=INSTALL_REQUIRES,
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 zip_safe=False,
+                 extras_require=EXTRAS_REQUIRE,
+                 cmdclass={
+                 })
