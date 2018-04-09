bd5c62314108939431813bb5e0bb67a8a9ef3dba
Shawn Dutton <shawn.dutton@rackspace.com>
2018-04-09 18:58:42 -0500

    QET-61: Add setup logging function

---
+++ qe_logging/qe_logging/__init__.py
@@ -0,0 +1,59 @@
+import logging
+import os
+from datetime import datetime
+
+
+DEFAULT_LOG_DIRECTORY = 'logs'
+
+
+def setup_logging(log_name_prefix, *historical_log_dir_layers, log_directory=DEFAULT_LOG_DIRECTORY):
+    '''
+    Will setup logging file handlers with a standard format for QE logging.
+
+    One handler will be set to write in the base log directory, the other in the historical log dir
+    layers (if provided) with time stamped directories.
+
+    Args:
+        log_name_prefix (str): The prefix for the log file name, prepended to .master.log.
+        *historical_log_dir_layers (str):   Additional directory layers if desired for the
+            historical log directories and files.
+        log_directory: (str) - The directory for the logs.
+
+    Examples:
+        >>> setup_logging('QE_LOGS', log_directory='logs_dir')
+        >>> import logging
+        >>> logging.getLogger('Bubba Logger').critical('Urgent Message!')
+        Writes files:
+        logs_dir/QE_LOGS.master.log
+            YYYY-MM-DD HH:MM:SS,FFF:CRITICAL:Bubba Logger             :Urgent Message!
+        logs_dir/YYYY-MM-DD_HH_MM_SS.FFFFFF/QE_LOGS.master.log
+            YYYY-MM-DD HH:MM:SS,FFF:CRITICAL:Bubba Logger             :Urgent Message!
+
+        >>> setup_logging('QET', 'some_layer', 'another_layer', log_directory='logs_dir')
+        >>> import logging
+        >>> logging.getLogger('SOME LOGGER').critical('LOOK AT ME')
+        Writes files:
+        logs_dir/QE_LOGS.master.log
+            YYYY-MM-DD HH:MM:SS,FFF:CRITICAL:SOME LOGGER              :LOOK AT ME
+        logs_dir/some_layer/another_layer/YYYY-MM-DD_HH_MM_SS.FFFFFF/QE_LOGS.master.log
+            YYYY-MM-DD HH:MM:SS,FFF:CRITICAL:SOME LOGGER              :LOOK AT ME
+    '''
+    root_log = logging.getLogger('')
+    # If a FileHandler logger has not been added, create it now
+    if not any(isinstance(x, logging.FileHandler) for x in root_log.handlers):
+        ts_dir = str(datetime.now()).replace(' ', '_').replace(':', '_')
+        log_dir = os.path.join(*[log_directory, *historical_log_dir_layers, ts_dir])
+        for dir_ in (log_dir, log_directory):
+            if not os.path.exists(dir_):
+                os.makedirs(dir_)
+            # Set up handler with encoding and msg formatter in log directory
+            # Set mode to 'w' so that the known file name log file is reset.
+            # Old logs still available in the timestamped directories.
+            log_handler = logging.FileHandler(
+                os.path.join(dir_, '{}.master.log'.format(log_name_prefix.lower())),
+                mode='w',
+                encoding='UTF-8'
+            )
+            formatter = logging.Formatter('{asctime}:{levelname:8}:{name:25}:{message}', style='{')
+            log_handler.setFormatter(formatter)
+            root_log.addHandler(log_handler)
---
+++ qe_logging/setup.py
@@ -0,0 +1,41 @@
+import os
+import setuptools
+
+NAME = 'qe_logging'
+DESCRIPTION = 'QE logging setup'
+VERSION = None
+
+CONSOLE_SCRIPTS = [
+]
+
+INSTALL_REQUIRES = [
+]
+
+EXTRAS_REQUIRE = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+setuptools.setup(name=NAME,
+                 version=about['__version__'],
+                 description=DESCRIPTION,
+                 url='https://github.rackspace.com/QualityEngineering/QE-Tools',
+                 author='Rackspace QE',
+                 author_email='qe-tools-contributors@rackspace.com',
+                 license='MIT',
+                 entry_points={
+                     'console_scripts': CONSOLE_SCRIPTS,
+                 },
+                 install_requires=INSTALL_REQUIRES,
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 zip_safe=False,
+                 extras_require=EXTRAS_REQUIRE,
+                 )
