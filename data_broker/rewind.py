dc32fd8bb34aa0397949302da831cb00f63ac703
Brad Brown <brad.brown@rackspace.com>
2018-04-20 14:31:39 -0500

    QET-64: pr tweaks

--- data_broker/rewind.py
+++ data_broker/rewind.py
@@ -1,33 +1,38 @@
 #! /usr/bin/env python
 
+import argparse
 import glob
 import json
-import os
 import lzma
+import os
 import sys
 import time
 
 import requests
 
+from app import PROD_DATA_DIR
+
 FILENAME_TIMESTAMP_FORMAT = '%Y%m%d_%H%M%S'
-USER_HOME = os.path.expanduser('~')
-LOCAL_POST_URL = 'http://localhost:6969/coverage/production?timestamp={}&is_rewind=True'
+POST_URL = 'https://qetools.rax.io/coverage/production'
 
 
-def main(*file_paths):
-    files = file_paths or glob.glob('{}/data_broker_files/*/*.xz'.format(USER_HOME))
-    for filename in files:
+def main(file_paths=None):
+    file_paths = file_paths or glob.glob('{}/*/*.xz'.format(PROD_DATA_DIR))
+    for filename in file_paths:
         epoch_stamp = time.mktime(time.strptime(
             filename.split('/')[-1].rstrip('.xz'), FILENAME_TIMESTAMP_FORMAT
         ))
         with lzma.open(filename) as f:
             data = json.loads(f.read())
         response = requests.post(
-            LOCAL_POST_URL.format(epoch_stamp), json=data
+            POST_URL, json=data, params={'timestamp': epoch_stamp, 'is_rewind': True}
         )
-        response.raise_for_status()
+        if response.status_code != 201:
+            print('Upload for "{}" failed!'.format(filename))
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:] if len(sys.argv) > 1 else []
-    main(*args)
+    parser = argparse.ArgumentParser('DataBroker Data Rewind')
+    parser.add_argument('files', nargs='*', help='Paths to files to rewind through the broker')
+    args = parser.parse_args()
+    main(args.files)
