a81cda60a3fbebb868787ffbd5a90be5aef6b960
Brad Brown <brad.brown@rackspace.com>
2018-04-23 06:42:14 -0500

    QET-64: code tweaks

--- data_broker/rewind.py
+++ data_broker/rewind.py
@@ -17,8 +17,7 @@ POST_URL = 'https://qetools.rax.io/coverage/production'
 
 
 def main(file_paths=None):
-    file_paths = file_paths or glob.glob('{}/*/*.xz'.format(PROD_DATA_DIR))
-    for filename in file_paths:
+    for filename in file_paths or glob.glob('{}/*/*.xz'.format(PROD_DATA_DIR)):
         epoch_stamp = time.mktime(time.strptime(
             filename.split('/')[-1].rstrip('.xz'), FILENAME_TIMESTAMP_FORMAT
         ))
@@ -33,6 +32,7 @@ def main(file_paths=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('DataBroker Data Rewind')
-    parser.add_argument('files', nargs='*', help='Paths to files to rewind through the broker')
+    help_msg = 'Paths to files to rewind through the broker. By default, all files will be run.'
+    parser.add_argument('files', nargs='*', help=help_msg)
     args = parser.parse_args()
     main(args.files)
