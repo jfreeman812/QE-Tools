29cf6f1b87a4431f8a2e5d4f9e79bf81b121c76f
Brad Brown <brad.brown@rackspace.com>
2018-05-08 14:01:35 -0500

    Enhancement: rename keys during rewind

--- data_broker/rewind.py
+++ data_broker/rewind.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import functools
 import glob
 import json
 import lzma
@@ -16,13 +17,31 @@ FILENAME_TIMESTAMP_FORMAT = '%Y%m%d_%H%M%S'
 POST_URL = 'https://qetools.rax.io/coverage/production'
 
 
-def main(file_paths=None):
+def _rename_entry(key, from_value, to_value, entry):
+    if entry.get(key) == from_value:
+        entry[key] = to_value
+
+
+def _prep_rename_function(rename):
+    key, from_value, to_value = rename.split(':')
+    return functools.partial(_rename_entry, key, from_value, to_value)
+
+
+def _rename_data(data, rename_funcs):
+    for entry in data:
+        for rename in rename_funcs:
+            rename(entry)
+
+
+def main(file_paths=None, renames=None):
     for filename in file_paths or glob.glob('{}/*/*.xz'.format(PROD_DATA_DIR)):
         epoch_stamp = time.mktime(time.strptime(
             filename.split('/')[-1].rstrip('.xz'), FILENAME_TIMESTAMP_FORMAT
         ))
         with lzma.open(filename) as f:
             data = json.loads(f.read())
+        if renames:
+            _rename_data(data, [_prep_rename_function(r) for r in renames])
         response = requests.post(
             POST_URL, json=data, params={'timestamp': epoch_stamp, 'is_rewind': True}
         )
@@ -35,5 +54,8 @@ if __name__ == '__main__':
     help_msg = ('Paths to files to rewind through the broker. '
                 'By default, all files in {} will be read.'.format(PROD_DATA_DIR))
     parser.add_argument('files', nargs='*', help=help_msg)
+    rename_help = ('Keys to rename in the format <key>:<from_string>:<to_string>.'
+                   '  Example: "Product Hierarchy:SPA:Scripted Parsing Analytics"')
+    parser.add_argument('--rename', '-r', action='append', help=rename_help)
     args = parser.parse_args()
-    main(args.files)
+    main(args.files, args.rename)
