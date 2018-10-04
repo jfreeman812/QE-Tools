181a175dc1a51e0a58e36e89c2fc123ac6064d09
doug9425 <Doug.Philips@rackspace.com>
2018-10-04 11:46:16 -0400

    Enhancement: Add dict filter/transform helpers.

--- qecommon_tools/tests/test_base.py
+++ qecommon_tools/tests/test_base.py
@@ -73,6 +73,69 @@ def test_dict_strip_stripped_values_are_gone(dict_to_strip, value_to_strip):
     assert value_to_strip not in stripped_dictionary.values()
 
 
+@pytest.mark.parametrize('dict_to_filter,value_to_keep', DIST_STRIP_CASES)
+def test_filter_dict_keep_value(dict_to_filter, value_to_keep):
+    new_dict = qecommon_tools.filter_dict(dict_to_filter, keep_value=lambda x: x == value_to_keep)
+    assert all(map(lambda x: x == value_to_keep, new_dict.values()))
+
+
+def test_filter_dict_noop():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_dict = qecommon_tools.filter_dict(a_dict)
+    assert new_dict == a_dict
+    assert new_dict is not a_dict
+
+
+def test_filter_dict_keep_key():
+    all_keys = [uuid4() for x in range(5)]  # arbitrary length > 1 and "not too big"
+    a_dict = {key: "value doesn't matter" for key in all_keys}
+    for key_to_keep in all_keys:
+        new_dict = qecommon_tools.filter_dict(a_dict, keep_key=lambda x: x == key_to_keep)
+        assert len(new_dict) == 1
+        assert key_to_keep in new_dict
+
+
+def test_filter_dict_keep_nothing_by_key():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_dict = qecommon_tools.filter_dict(a_dict, keep_key=qecommon_tools.always_false)
+    assert new_dict == {}
+
+
+def test_filter_dict_keep_nothing_by_value():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_dict = qecommon_tools.filter_dict(a_dict, keep_value=qecommon_tools.always_false)
+    assert new_dict == {}
+
+
+def test_filter_dict_keep_nothing_by_key_and_value():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_dict = qecommon_tools.filter_dict(a_dict, keep_key=qecommon_tools.always_false,
+                                          keep_value=qecommon_tools.always_false)
+    assert new_dict == {}
+
+
+def test_dict_transform_noop():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_dict = qecommon_tools.dict_transform(a_dict)
+    assert new_dict == a_dict
+
+
+def test_dict_transform_monokey():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_key = uuid4()
+    new_dict = qecommon_tools.dict_transform(a_dict, key_transform=lambda x: new_key)
+    assert len(new_dict) == 1
+    assert new_key in new_dict
+
+
+def test_dict_transform_monovalue():
+    a_dict = {uuid4(): uuid4() for x in range(5)}  # arbitrary length > 1 and "not too big"
+    new_value = uuid4()
+    new_dict = qecommon_tools.dict_transform(a_dict, value_transform=lambda x: new_value)
+    assert len(new_dict) == len(a_dict)
+    assert set(new_dict.values()) == {new_value}
+
+
 PADDED_LIST_CASES = list(product([0, 5, 15], [None, 'TEST']))
 
 
